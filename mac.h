efbdcc2da3c2e9f5c7bf693471c9cfb9cac86290
Ari Zablozki <ari@marina59.com>
2026-03-24 09:07:40 -0400

    added mac.h

---
+++ mac.h
@@ -0,0 +1,60 @@
+static enum colors DEF_BG;
+static enum colors DEF_FG;
+
+typedef struct cell {
+    char ch;
+    struct {
+        size_t seqLen;
+        char pos[32];
+    } cursor;
+    struct {
+        size_t len;
+        char *color;
+    } fg;
+    struct {
+        size_t len;
+        char *color;
+    } bg;
+    struct {
+        size_t len;
+        char *val;
+    } attributes;
+} cell;
+
+typedef struct grid {
+    int nCols;
+    int nRows;
+     cell cell[];
+} grid;
+
+typedef struct {
+    int len;
+    char data[];
+} frameBuffer;
+
+grid *initGrid(int, int);
+grid *resizeGrid(grid*, struct termConfig*);
+void setCursorPostions(grid*);
+void clearAllGridCells(grid*, char);
+int writeToGrid(grid*, int(*screen)(grid *g));
+size_t countBytes(grid*);
+
+frameBuffer *initFrameBuffer(void);
+void serializeGrid(grid*, frameBuffer*);
+void bltFrameBuffer(frameBuffer*);
+
+int xCenter(grid*, int);
+int xThird(grid*, int) ;
+void setFgCl(grid*, int, int, enum colors);
+void setBgCl(grid*, int, int, enum colors);
+void setChar(grid*, int, int, char);
+void underline(grid*, int, int, int, enum colors, enum colors);
+void hText(grid*, char*, int, int, enum colors, enum colors);
+
+void handler(int);
+void setDefaultColors(enum colors, enum colors);
+void term_send_pos(int, int);
+
+
+
+
