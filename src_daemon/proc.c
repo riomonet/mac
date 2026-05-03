a1e855b8fc39939fe0d8f3decd542f2061ec27de
Ari Zablozki <ari@marina59.com>
2026-05-03 00:23:40 -0400

    added naming conventions mck_proc_t

--- src_daemon/proc.c
+++ src_daemon/proc.c
@@ -1,41 +1,66 @@
-struct mac_proc proc_table[PROC_TABLE_SIZE] = {0};
-  	
-int proc_add_to_table(int proc_sockfd) {
-    int i = 0;
-    for(; i < PROC_TABLE_SIZE; i++) {
-	if (proc_table[i].fd != -1) {  // find the next available slot
+mck_proc_t mck_proc_array[MCK_PROC_ARRAY_SIZE] = {0};
+
+const mck_proc_t mck_proc_default = {
+    .fd = PROC_ARRAY_SLOT_AVAILABLE,
+    .user = {0},
+    .uid = -1,
+    .gid = -1,
+    .screen_state = -1,
+    // if its in the proc table its logged in
+    // logged_in_time
+    // login attempts
+    // timeout
+    /* pass the proc  */
+};
+
+int mck_proc_array_add(int proc_sockfd) {
+    int i;
+    for(i = 0; i < MCK_PROC_ARRAY_SIZE; i++) {
+	if (PROC_ARRAY_SLOT_OCCUPIED(mck_proc_array[i])) {   // find the next available slot
 	    continue;
 	}
-	proc_table[i].fd = proc_sockfd;
+	PROC_FD(i) = proc_sockfd;
 	return i;
     }
-    return PROC_ERROR_TABLE;
+    return PROC_ARRAY_ERROR;
 }
 
-int proc_remove_from_table(int proc_sockfd) {
-    for (int i = 0; i <  PROC_TABLE_SIZE; i++) {
-	if (proc_table[i].fd == proc_sockfd) {
-	    proc_table[i].fd = -1;
+/* Thid function finds a recod in 'proc_array' by 'proc_state.fd'
+ * and resets it to default value, using the 'proc_state_default'
+ * template. This makes the record INACTIVE*/
+int mck_proc_array_remove(int proc_sockfd) {
+    int i;
+    for (i = 0; i <  MCK_PROC_ARRAY_SIZE; i++) {
+	if (PROC_FD(i) == proc_sockfd) {
+	    mck_proc_array[i] = mck_proc_default;
 	    return 0;
 	}
     }
-    return PROC_ERROR_NOT_FOUND; // NOT FOUND
+    return PROC_NOT_FOUND_ERROR; // NOT FOUND
 }
 
-void proc_initialize_table() {
-    for (int i = 0; i < PROC_TABLE_SIZE; i++) {
-	proc_table[i].fd = -1;
+
+/* This function sets every 'proc_state' record in 'proc_array' to
+ * a default state, defined in 'proc_state_default' */
+void proc_array_initialize() {
+    int i;
+    for (i = 0; i < MCK_PROC_ARRAY_SIZE; i++) {
+	mck_proc_array[i] = mck_proc_default; 
     }
 }
 
-void proc_refresh_table(fd_set *fd_set_ptr) {
-  FD_ZERO(fd_set_ptr);
-  int i = 0;
-  for(; i < PROC_TABLE_SIZE; i++) {
-	if (proc_table[i].fd != -1) {
-      FD_SET(proc_table[i].fd, fd_set_ptr);
+/* When using 'select' it is necessary to refresh the kernel fd_set every iteration
+ * through the main loop. This function clears the kernel fd_set and then adds 
+ * back the 'proc_state.fd' from the 'proc_array' table that
+ * are still active */
+void proc_array_refresh(fd_set *fd_set_ptr) {
+    int i;
+    FD_ZERO(fd_set_ptr); // Zero out the select set so there are no fd's
+    for(i = 0; i < MCK_PROC_ARRAY_SIZE; i++) {
+	if (PROC_ARRAY_SLOT_OCCUPIED(mck_proc_array[i]) ) {
+	    FD_SET(PROC_FD(i), fd_set_ptr); // add active proc_array proc_records to the fd_set
 	}
-  }
+    }
 }
 
 	
--- src_daemon/proc.h
+++ src_daemon/proc.h
@@ -1,28 +1,31 @@
 #ifndef PROC_H
 #define PROC_H
 
-#define PROC_TABLE_SIZE 32
-#define PROC_ERROR_TABLE -1
-#define PROC_ERROR_NOT_FOUND -1
+#define MCK_PROC_ARRAY_SIZE 128  //table of procs
+#define PROC_ARRAY_ERROR -1
+#define PROC_NOT_FOUND_ERROR -1
+
+#define PROC_ARRAY_SLOT_AVAILABLE -1
+#define PROC_ARRAY_SLOT_OCCUPIED(slot) ((slot).fd != -1)
+#define PROC_FD(idx) (mck_proc_array[(idx)].fd)
 // new client connects, we need to track connections.
 // master_file_desciprtor is server_sockfd
 // data_sockfd are clients
 
-struct mac_proc {
+typedef struct mck_proc_s {
     int fd;
     char user[16];
     int uid;
     int gid;
-    int current_state;
+    int screen_state;
     // if its in the proc table its logged in
     // logged_in_time
     // login attempts
     // timeout
     /* pass the proc  */
-};
-
-extern struct mac_proc proc_table[PROC_TABLE_SIZE];
+} mck_proc_t;
 
 
+extern mck_proc_t mck_proc_array[MCK_PROC_ARRAY_SIZE];
 
 #endif
